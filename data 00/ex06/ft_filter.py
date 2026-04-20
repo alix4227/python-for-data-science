0b6250379828b05ca362cced4ee43e8f8346c0c3
alix <alix.crusoe@gmail.com>
2026-04-20 13:50:40 +0200

    fix ex06

--- data 00/ex06/ft_filter.py
+++ data 00/ex06/ft_filter.py
@@ -13,10 +13,13 @@ def ft_filter(function, iterable):
 
 
 def main():
-    test = ["lechat", "lechien"]
-    print(ft_filter(None,test))
-    # print(filter.__doc__)
+    test = [1, 10, 15, 8, 5]
 
+    print(list(filter(None, test)))
+    print(list(ft_filter(None, test)))
+
+    print(list(filter(lambda s: s % 2 == 0, test)))
+    print(list(ft_filter(lambda s: s % 2 == 0, test)))
 
 if __name__ == "__main__":
     main()
