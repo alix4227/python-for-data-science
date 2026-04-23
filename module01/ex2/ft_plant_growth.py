13f8f8bf6a558c93327bf4ccade171c077a6df9c
alix <alix.crusoe@gmail.com>
2026-04-23 17:49:53 +0200

    private attributes

--- module01/ex2/ft_plant_growth.py
+++ module01/ex2/ft_plant_growth.py
@@ -3,41 +3,43 @@ import sys
 class Plant:
     
     def __init__(self, name: str, height: float, age: int, growth: float):
-        self.name: str = name
         if height < 0 or age < 0 or growth < 0:
             raise ValueError("value error")
         if not isinstance(name, str) or not name.strip():
             raise ValueError("Not a string")
-        self.height: float = height
-        self.age_days: int = age
-        self.total_growth: float = 0
-        self.growth: float = growth
+        self.__name: str = name
+        self.__height: float = height
+        self.__age_days: int = age
+        self.__total_growth: float = 0
+        self.__growth: float = growth
     
     def show(self):
         print('=== Garden Plant Growth ===')
-        print(f'{self.name}: {round(self.height, 2)}cm, {self.age_days} days old')
+        print(f'{self.__name}: {round(self.__height, 2)}cm, {self.__age_days} days old')
         for i in range(1, 8):
-            self.grow(self.growth)
-            self.total_growth += self.growth
+            self.grow(self.__growth)
+            self.__total_growth += self.__growth
             self.age(1)
             self.showGrowth(i)
-        print(f'Growth this week: {self.total_growth}cm')
+        print(f'Growth this week: {self.__total_growth}cm')
     
     def showGrowth(self, day):
         print(f'=== Day {day} ===')
-        print(f'{self.name}: {round(self.height, 2)}cm, {self.age_days} days old')
+        print(f'{self.__name}: {round(self.__height, 2)}cm, {self.__age_days} days old')
     
     def grow(self, i: float):
-        self.height += i
+        self.__height += i
 
     def age(self, i: int):
-        self.age_days += i
+        self.__age_days += i
         
 
 
 def main(args):
     try:
-        rose = Plant("", 25, 30, 0.8)
+        rose = Plant("Rose", 25, 30, 0.8)
+        rose.show()
+        rose.__name = "Lechat"
         rose.show()
     except ValueError as e:
         print(f'{e}')
