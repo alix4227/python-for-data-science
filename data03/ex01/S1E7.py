e4eae5f4c22478667cf5941505fea52a29120043
alix <alix.crusoe@gmail.com>
2026-05-21 11:50:33 +0200

    ex00 + creation ex01

---
+++ data03/ex01/S1E7.py
@@ -0,0 +1,13 @@
+from S1E9 import Character
+class Baratheon(Character):
+    def __init__(self, first_name, is_alive=True):
+        """Your docstring for Constructor"""
+        super().__init__(first_name, is_alive)
+        self.family_name = 'Baratheon'
+        self.eyes = 'brown'
+        self.hairs = 'dark'
+class Lannister(Character):
+    #your code here
+# decorator
+def create_lannister(your code here):
+    #your code here
