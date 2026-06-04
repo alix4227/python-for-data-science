c310232d8c64b99ea975df7e0e981ee299904f09
alix <alix.crusoe@gmail.com>
2026-06-04 15:19:38 +0200

    ADD EX02 FINI

--- django/base/project/d04/settings.py
+++ django/base/project/d04/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOG_FILE = BASE_DIR / "ex02/logs.txt"
--- django/base/project/ex02/views.py
+++ django/base/project/ex02/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from ex02.forms import LoginForm
+from django.conf import settings
+import os
 def form(request):
     title = 'Text Area'
+    logs = []
+    if os.path.exists(settings.LOG_FILE):
+        with open(settings.LOG_FILE, 'r') as file2:
+            logs = file2.readlines()
     if request.method == "POST":
-      MyLoginForm = LoginForm(request.POST)
+        MyLoginForm = LoginForm(request.POST)
       
-      if MyLoginForm.is_valid():
-        text = MyLoginForm.cleaned_data['text']
-        timestamp = timezone.now().strftime('%Y-%m-%d %H:%M:%S')
-        with open("ex02/logs.txt", 'a') as file:
-            file.write(f"[{timestamp}]{text+'\n'}")
-
+        if MyLoginForm.is_valid():
+            text = MyLoginForm.cleaned_data['text']
+            timestamp = timezone.now().strftime('%Y-%m-%d %H:%M:%S')
+            with open(settings.LOG_FILE, 'a') as file:
+                file.write(f"[{timestamp}]{text+'\n'}")
+            with open(settings.LOG_FILE, 'r') as file2:
+                logs = file2.readlines()
+            MyLoginForm = LoginForm()
+            return redirect('form')
     else:
         MyLoginForm = LoginForm()
-    return render(request, 'ex02/form.html', {"title": title, "form": MyLoginForm})
+    return render(request, 'ex02/form.html', {"title": title, "form": MyLoginForm, "logs": logs})
