257a472fb35f09f94c50e564a995cd21e5723571
alix <alix.crusoe@gmail.com>
2026-06-05 18:22:48 +0200

    ex03 fini

--- django/base/project/ex03/views.py
+++ django/base/project/ex03/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 def gradient(request):
     title = 'Gradient'
-    return render(request, 'ex03/gradient.html', {"title": title, "range": range(50), "rgb": "rgb(0,0,0)"})
+    table = []
+    j = 255
+    b = 0
+
+    for i in range(50):
+        table.append([f"rgb({b}, {b}, {b})", f"rgb({j}, 0, 0)", f"rgb({b}, 0, {j})", f"rgb(0, {j}, {b})"])
+        j -= 4
+        b += 4
+    return render(request, 'ex03/gradient.html', {"title": title, "table": table})
