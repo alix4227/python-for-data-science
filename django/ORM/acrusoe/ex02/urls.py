0f80489560d85b5c9b3aae0dd6a6ad6ceb79c881
alix <alix.crusoe@gmail.com>
2026-06-11 18:10:40 +0200

    ADD ex02

--- django/ORM/acrusoe/ex02/urls.py
+++ django/ORM/acrusoe/ex02/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for d2 project.
+URL configuration for Django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("ex00/", include("ex00.urls")),
-    path("ex02/", include("ex02.urls")),
+    path("init", views.init, name="init"),
+    path("populate", views.populate, name="populate"),
+    path("display", views.display, name="display"),
 ]
---
+++ django/ORM/acrusoe/ex02/views.py
@@ -0,0 +1,113 @@
+from django.shortcuts import render
+import psycopg2
+
+def init(request):
+    conn = None
+    try:
+        conn = psycopg2.connect(
+        dbname="formationdjango",
+        user="djangouser",
+        password="secret",
+        host="localhost",
+        port="5432"
+    )
+
+        cur = conn.cursor()
+
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS ex02_movies (
+                title           VARCHAR(100) NOT NULL UNIQUE,
+                episode_nb      INTEGER PRIMARY KEY,
+                opening_crawl   TEXT,
+                director        VARCHAR(32) NOT NULL,
+                producer        VARCHAR(128) NOT NULL,
+                release_date    DATE NOT NULL
+            )
+        """)
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS ex02_movies (
+                title           VARCHAR(100) NOT NULL UNIQUE,
+                episode_nb      INTEGER PRIMARY KEY,
+                opening_crawl   TEXT,
+                director        VARCHAR(32) NOT NULL,
+                producer        VARCHAR(128) NOT NULL,
+                release_date    DATE NOT NULL
+            )
+        """)
+        conn.commit()
+        result = 'OK'
+        cur.close()
+        conn.close()
+   
+    except psycopg2.Error as e:
+        if conn:
+            conn.rollback()
+        result = e
+    return render(request, 'ex00/index.html', {"result": result})
+
+def populate(request):
+    conn = None
+    try:
+        conn = psycopg2.connect(
+        dbname="formationdjango",
+        user="djangouser",
+        password="secret",
+        host="localhost",
+        port="5432"
+    )
+
+        cur = conn.cursor()
+        movies = [
+        ('The Phantom Menace',        1, 'George Lucas',    'Rick McCallum',                                       '1999-05-19'),
+        ('Attack of the Clones',      2, 'George Lucas',    'Rick McCallum',                                       '2002-05-16'),
+        ('Revenge of the Sith',       3, 'George Lucas',    'Rick McCallum',                                       '2005-05-19'),
+        ('A New Hope',                4, 'George Lucas',    'Gary Kurtz, Rick McCallum',                           '1977-05-25'),
+        ('The Empire Strikes Back',   5, 'Irvin Kershner',  'Gary Kutz, Rick McCallum',                           '1980-05-17'),
+        ('Return of the Jedi',        6, 'Richard Marquand','Howard G. Kazanjian, George Lucas, Rick McCallum',   '1983-05-25'),
+        ('The Force Awakens',         7, 'J. J. Abrams',    'Kathleen Kennedy, J. J. Abrams, Bryan Burk',         '2015-12-11'),
+        ]
+        cur.executemany("""
+            INSERT INTO ex02_movies (title, episode_nb, director, producer, release_date)
+            VALUES (%s, %s, %s, %s, %s);
+        """, movies)
+        conn.commit()
+        result = 'OK'
+        cur.close()
+        conn.close()
+   
+    except psycopg2.Error as e:
+        if conn:
+            conn.rollback()
+        result = e
+    return render(request, 'ex00/index.html', {"result": result})
+
+def display(request):
+    conn = None
+    table = ['title','episode_nb','opening_crawl', 'director', 'producer', 'release_date']
+    try:
+        conn = psycopg2.connect(
+        dbname="formationdjango",
+        user="djangouser",
+        password="secret",
+        host="localhost",
+        port="5432"
+    )
+
+        cur = conn.cursor()
+        cur.execute("""
+            SELECT *
+            FROM ex02_movies;
+        """)
+        rows = cur.fetchall()
+        for i in range(4):
+            table[i] = [row[i] for row in rows]
+            print(table[1])
+        cur.close()
+        conn.close()
+        result = 'OK'
+   
+    except psycopg2.Error as e:
+        if conn:
+            conn.rollback()
+        result = e
+    return render(request, 'ex02/index.html', {"result": result, "table": table})
