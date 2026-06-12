7c836da3c4270dcc1cb989141fa82bea2dcd6828
alix <alix.crusoe@gmail.com>
2026-06-12 15:07:46 +0200

    ADD ex03 fini

---
+++ django/ORM/acrusoe/ex03/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+from ex01.models import *
+
+def populate(request):
+    results = []
+    films = [
+        Movies(episode_nb=1, title='The Phantom Menace', director='George Lucas',
+            producer='Rick McCallum', release_date='1999-05-19'),
+        Movies(episode_nb=2, title='Attack of the Clones', director='George Lucas',
+            producer='Rick McCallum', release_date='2002-05-16'),
+        Movies(episode_nb=3, title='Revenge of the Sith', director='George Lucas',
+            producer='Rick McCallum', release_date='2005-05-19'),
+        Movies(episode_nb=4, title='A New Hope', director='George Lucas',
+            producer='Gary Kurtz, Rick McCallum', release_date='1977-05-25'),
+        Movies(episode_nb=5, title='The Empire Strikes Back', director='Irvin Kershner',
+            producer='Gary Kutz, Rick McCallum', release_date='1980-05-17'),
+        Movies(episode_nb=6, title='Return of the Jedi', director='Richard Marquand',
+            producer='Howard G. Kazanjian, George Lucas, Rick McCallum', release_date='1983-05-25'),
+        Movies(episode_nb=7, title='The Force Awakens', director='J. J. Abrams',
+            producer='Kathleen Kennedy, J. J. Abrams, Bryan Burk', release_date='2015-12-11'),
+    ]
+    for film in films:
+        try:
+            film.save()
+            results.append('OK')
+        except Exception as e:
+            results.append(str(e))
+    return render(request, 'ex03/populate.html', {"results": results})
+
+def display(request):
+    result = 'No data available'
+    films = []
+    films = Movies.objects.all()
+    headers = ['episode_nb','title','director', 'producer', 'release_date', 'opening_crawl']
+    return render(request, 'ex03/display.html', {"films": films, "result": result, "headers": headers})
+
+    # blockbuster = Movies.objects.get(episode_nb=1)
+    # blockbuster.delete()
+   
