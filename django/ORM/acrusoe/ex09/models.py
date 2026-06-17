0c6c9605eaa885726837bf7c0ce4741228ce19d1
alix <alix.crusoe@gmail.com>
2026-06-17 15:54:08 +0200

    ADD fix ex09  models.py views.py

--- django/ORM/acrusoe/ex09/models.py
+++ django/ORM/acrusoe/ex09/models.py
@@ -2,17 +2,35 @@ from django.db import models
 
 class Planets(models.Model):
     name = models.CharField(max_length=64, unique=True, null=False)
-    climate = models.CharField()
+    climate = models.CharField(null=True, blank=True)
     diameter = models.IntegerField(null=True, blank=True)
     orbital_period = models.IntegerField(null=True, blank=True)
-    population = models.BigIntegerField(max_length=128)
-    rotation_period = models.IntegerFieldField()
-    surface_water = models.RealField()
-    terrain = models.CharField()
+    population = models.BigIntegerField(null=True, blank=True)
+    rotation_period = models.IntegerField(null=True, blank=True)
+    surface_water = models.FloatField(null=True, blank=True)
+    terrain = models.CharField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return self.title
+        return self.name
 
     class Meta:
-        db_table = "ex07_Movies"
+        db_table = "ex09_Planets"
+
+
+class People(models.Model):
+    name = models.CharField(max_length=64, unique=True, null=False)
+    birth_year = models.CharField(max_length=64, null=True, blank=True)
+    gender = models.CharField(max_length=32, null=True, blank=True)
+    eye_color = models.CharField(max_length=32, null=True, blank=True)
+    hair_color = models.CharField(max_length=32, null=True, blank=True)
+    heigth = models.IntegerField(null=True, blank=True)
+    mass = models.FloatField(null=True, blank=True)
+    homeworld = models.ForeignKey(Planets, to_field= 'name', on_delete=models.CASCADE, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "ex09_People"
--- django/ORM/acrusoe/ex09/views.py
+++ django/ORM/acrusoe/ex09/views.py
@@ -2,56 +2,55 @@ from django.shortcuts import render
 
 from ex09.models import *
 import datetime
-
-def populate(request):
-    results = []
-    films = [
-        Movies(episode_nb=1, title='The Phantom Menace', director='George Lucas',
-            producer='Rick McCallum', release_date='1999-05-19'),
-        Movies(episode_nb=2, title='Attack of the Clones', director='George Lucas',
-            producer='Rick McCallum', release_date='2002-05-16'),
-        Movies(episode_nb=3, title='Revenge of the Sith', director='George Lucas',
-            producer='Rick McCallum', release_date='2005-05-19'),
-        Movies(episode_nb=4, title='A New Hope', director='George Lucas',
-            producer='Gary Kurtz, Rick McCallum', release_date='1977-05-25'),
-        Movies(episode_nb=5, title='The Empire Strikes Back', director='Irvin Kershner',
-            producer='Gary Kutz, Rick McCallum', release_date='1980-05-17'),
-        Movies(episode_nb=6, title='Return of the Jedi', director='Richard Marquand',
-            producer='Howard G. Kazanjian, George Lucas, Rick McCallum', release_date='1983-05-25'),
-        Movies(episode_nb=7, title='The Force Awakens', director='J. J. Abrams',
-            producer='Kathleen Kennedy, J. J. Abrams, Bryan Burk', release_date='2015-12-11'),
-    ]
-    for film in films:
-        try:
-            film.save()
-            results.append('OK')
-        except Exception as e:
-            results.append(str(e))
-    return render(request, 'ex09/populate.html', {"results": results})
+import csv
 
 def display(request):
-    result = 'No data available'
-    films = []
-    films = Movies.objects.all()
-    headers = ['episode_nb','title','director', 'producer', 'release_date', 'opening_crawl', 'created', 'updated']
-    return render(request, 'ex09/display.html', {"films": films, "result": result, "headers": headers})
+    result = f'No data available,please use the following command line before use:'
+    planets = []
+    colonnes_planets = ['name', 'climate', 'diameter', 'orbital_period', 'population', 'rotation_period', 'surface_water', 'terrain']
+    colonnes_people = ['name', 'birth_year', 'gender', 'eye_color', 'hair_color', 'heigth', 'mass', 'homeworld']
 
+    with open('ex09/planets.csv', newline='') as f:
+        reader = csv.DictReader(f, fieldnames=colonnes_planets, delimiter='\t')
+        for row in reader:
+            for col in colonnes_planets:
+                if row[col] == 'NULL':
+                    row[col] = None
+            Planets.objects.get_or_create(
+            name=row['name'],
+            defaults={
+                'climate': row['climate'],
+                'diameter': row['diameter'],
+                'orbital_period': row['orbital_period'],
+                'population': row['population'],
+                'rotation_period': row['rotation_period'],
+                'surface_water': row['surface_water'],
+                'terrain': row['terrain'] 
+            }
+        )
+    with open('ex09/people.csv', newline='') as f:
+        reader = csv.DictReader(f, fieldnames=colonnes_people, delimiter='\t')
+        for row in reader:
+            for col in colonnes_people:
+                if row[col] == 'NULL':
+                    row[col] = None
+            if row['homeworld'] is None:
+                continue
+            planet = Planets.objects.get(name=row['homeworld'])
+            People.objects.get_or_create(
+            name=row['name'],
+            defaults={
+                'birth_year': row['birth_year'],
+                'gender': row['gender'],
+                'eye_color': row['eye_color'],
+                'hair_color': row['hair_color'],
+                'heigth': row['heigth'],
+                'mass': row['mass'],
+                'homeworld': planet 
+            }
+        )
+    people = People.objects.filter(homeworld__climate__icontains='windy').order_by('name')
+    headers = ['name','homeworld','climate']
+    return render(request, 'ex09/display.html', {"people": people, "result": result, "headers": headers})
 
-def update(request):
-    titles = []
-    result = ''
-    try:
-        if request.method == "POST":
-            value = request.POST.get('movies')
-            input = request.POST.get('input')
-            movie = Movies.objects.get(title=value)
-            movie.opening_crawl = input
-            movie.updated = datetime.datetime.now()
-            movie.save()
-        titles = Movies.objects.values_list('title', flat=True)
-        result = "No data available"
-        
-    except Exception:
-        result = "No data available"
-    return render(request, 'ex09/update.html', {"result": result, "titles": titles})
    
