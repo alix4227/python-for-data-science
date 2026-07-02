a33e0ce04d24cb9028cea020cd72e50021544e4d
alix4227 <alix_07@hotmail.fr>
2026-07-02 13:17:06 +0200

    ADD chat fini

--- django/djangoFinal/chat/consumers.py
+++ django/djangoFinal/chat/consumers.py
@@ -4,13 +4,18 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 import re
 
-from .models import Chatroom, Message
+from .models import Chatroom, Message,User
 
 class ChatConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
     def get_messages(self):
         chatroom = Chatroom.objects.get(name=self.room_name)
-        return list(Message.objects.filter(chatroom=chatroom).values('user__username', 'content'))
+        return list(Message.objects.filter(chatroom=chatroom).order_by('created').values('user__username', 'content'))
+    
+    @database_sync_to_async
+    def get_members_connected(self):
+        chatroom = Chatroom.objects.get(name=self.room_name)
+        return list(chatroom.members.values_list('username', flat=True))
 
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
@@ -18,20 +23,39 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-
+        await self.save_connected_members()
         messages = await self.get_messages()
-        messages = [f"{message['user__username']}: {message['content']}" for message in messages]
+        members_connected = await self.get_members_connected()
+        messages = [f"{message['user__username']}: {message['content']}" for message in messages][-3:]
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "messages": messages,
+                    "members_connected": members_connected,
+                }
+            )
+        )
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "chat.message",
                 "message": f"{self.scope['user']} has joined the chat",
-                "messages": messages,
+                "members_connected": members_connected,
             }
         )
 
     async def disconnect(self, close_code):
-        # Leave room group
+        await self.remove_connected_members()
+        members_connected = await self.get_members_connected()
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat.message",
+                "message": f"{self.scope['user']} has left the chat",
+                "members_connected":members_connected,
+            }
+        )
+        # Leave room group after broadcasting the departure.
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
@@ -54,10 +78,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
             chatroom=chatroom,
             content=message,
         )
-
+    @database_sync_to_async
+    def save_connected_members(self):
+        chatroom = Chatroom.objects.get(name=self.room_name)
+        chatroom.members.add(self.scope["user"])
+    @database_sync_to_async
+    def remove_connected_members(self):
+        chatroom = Chatroom.objects.get(name=self.room_name)
+        chatroom.members.remove(self.scope["user"])
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message, "messages": event.get("messages", []),}))
+        await self.send(text_data=json.dumps({"message": message, "messages": event.get("messages", []),"members_connected": event.get("members_connected", []),}))
