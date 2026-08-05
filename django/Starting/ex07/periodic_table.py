1805f3d0ecdbc4ad39d1914fe731fc0cb3d9aaad
alix4227 <alix_07@hotmail.fr>
2026-08-05 10:54:21 +0200

    ADD module 01 fini

--- django/Starting/ex07/periodic_table.py
+++ django/Starting/ex07/periodic_table.py
@@ -1,5 +1,56 @@
 import sys
 
+POSITIONS = {
+    1: (1, 1), 2: (1, 18),
+    3: (2, 1), 4: (2, 2), 5: (2, 13), 6: (2, 14), 7: (2, 15), 8: (2, 16), 9: (2, 17), 10: (2, 18),
+    11: (3, 1), 12: (3, 2), 13: (3, 13), 14: (3, 14), 15: (3, 15), 16: (3, 16), 17: (3, 17), 18: (3, 18),
+    19: (4, 1), 20: (4, 2), 21: (4, 3), 22: (4, 4), 23: (4, 5), 24: (4, 6), 25: (4, 7),
+    26: (4, 8), 27: (4, 9), 28: (4, 10), 29: (4, 11), 30: (4, 12),
+    31: (4, 13), 32: (4, 14), 33: (4, 15), 34: (4, 16), 35: (4, 17), 36: (4, 18),
+    37: (5, 1), 38: (5, 2), 39: (5, 3), 40: (5, 4), 41: (5, 5), 42: (5, 6), 43: (5, 7),
+    44: (5, 8), 45: (5, 9), 46: (5, 10), 47: (5, 11), 48: (5, 12),
+    49: (5, 13), 50: (5, 14), 51: (5, 15), 52: (5, 16), 53: (5, 17), 54: (5, 18),
+    55: (6, 1), 56: (6, 2),
+    72: (6, 4), 73: (6, 5), 74: (6, 6), 75: (6, 7), 76: (6, 8), 77: (6, 9), 78: (6, 10),
+    79: (6, 11), 80: (6, 12), 81: (6, 13), 82: (6, 14), 83: (6, 15), 84: (6, 16), 85: (6, 17), 86: (6, 18),
+    87: (7, 1), 88: (7, 2),
+    104: (7, 4), 105: (7, 5), 106: (7, 6), 107: (7, 7), 108: (7, 8), 109: (7, 9), 110: (7, 10),
+    111: (7, 11), 112: (7, 12), 113: (7, 13), 114: (7, 14), 115: (7, 15), 116: (7, 16), 117: (7, 17), 118: (7, 18)
+}
+
+def build_grid(list_elements):
+    grid = {} 
+    for elem in list_elements:
+        row, col = POSITIONS[int(elem['atomic_number'])]
+        grid[(row, col)] = elem
+    return grid
+
+def render_cell(elem):
+    if elem is None:
+        return "<td style=\"border: 1px; padding:10px\"></td>\n"
+    return (
+        "<td style=\"border: 1px solid black; padding:10px\">\n"
+        f"  <h4>{elem['name']}</h4>\n"
+        "  <ul>\n"
+        f"    <li>{elem['atomic_number']}</li>\n"
+        f"    <li>{elem['symbol']}</li>\n"
+        f"    <li>{elem['atomic_mass']}</li>\n"
+        "  </ul>\n"
+        "</td>\n"
+    )
+
+def build_html(list_elements):
+    grid = build_grid(list_elements)
+    html = "<!DOCTYPE html>\n<html lang=\"en\">\n  <head>\n"
+    html += "    <meta charset=\"utf-8\">\n    <title>Periodic Table</title>\n  </head>\n"
+    html += "  <body>\n      <table>\n      <tbody>\n"
+    for row in range(1, 8):
+        html += "        <tr>\n"
+        for col in range(1, 19):
+            html += render_cell(grid.get((row, col)))
+        html += "        </tr>\n"
+    html += "      </tbody>\n    </table>\n  </body>\n</html>\n"
+    return html
 
 def main(args):
     dict_elements = {}
@@ -18,58 +69,7 @@ def main(args):
                 else:
                     dict_elements[keys[i]] = elements[i].split(':')[1].strip()
             list_elements.append(dict_elements)
-    html = "<!DOCTYPE html>\n"
-    html += "<html lang=\"en\">\n"
-    html += "  <head>\n"
-    html += "    <meta charset=\"utf-8\">\n"
-    html += "    <title>Periodic Table</title>\n"
-    html += "  </head>\n"
-    html += "  <body>\n"
-    html += "    <table>\n"
-    html += "      <tbody>\n"
-    i = 0
-    while i < 2:
-        html += "        <tr>\n"
-        j = 0
-        k = 0
-        while j < 18:
-            if j == 0 or j == 17:
-                elem = list_elements[k]
-                html += "          <td style=\"border: 1px solid black; padding:10px\">\n"
-                html += f"            <h4>{elem['name']}</h4>\n"
-                html += "            <ul>\n"
-                html += f"              <li>{elem['atomic_number']}</li>\n"
-                html += f"              <li>{elem['symbol']}</li>\n"
-                html += f"              <li>{elem['atomic_mass']}</li>\n"
-                html += "            </ul>\n"
-                html += "          </td>\n"
-                k += 1
-            else:
-                html += "          <td style=\"border: 1px black; padding:10px\">\n"
-                html += "          </td>\n"
-            j += 1    
-        html += "        </tr>\n"
-        i += 2
-    while i < len(list_elements):
-        html += "        <tr>\n"
-        j = 0
-        while j < 18 and i + j < len(list_elements):
-            elem = list_elements[i + j]
-            html += "          <td style=\"border: 1px solid black; padding:10px\">\n"
-            html += f"            <h4>{elem['name']}</h4>\n"
-            html += "            <ul>\n"
-            html += f"              <li>{elem['atomic_number']}</li>\n"
-            html += f"              <li>{elem['symbol']}</li>\n"
-            html += f"              <li>{elem['atomic_mass']}</li>\n"
-            html += "            </ul>\n"
-            html += "          </td>\n"
-            j += 1
-        html += "        </tr>\n"
-        i += 18
-    html += "      </tbody>\n"
-    html += "    </table>\n"
-    html += "  </body>\n"
-    html += "</html>\n"
+    html = build_html(list_elements)
     with open("resultat.html", "w") as f:
         f.write(html)
 if __name__ == '__main__':
